af072624aa19f8a329a510fdc00f8516a3c2d24a
Vladimir Kurganov <err0rfrost@gmail.com>
2023-06-14 00:50:27 +0600

    feat: autoscroll to top after change page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import AutoScrollToTop from '../../helpers/AutoScrollToTop';
 import HeaderBar from './HeaderBar';
 import Logo from './Logo';
 
@@ -6,6 +7,7 @@ export default function Header() {
         <>
             <Logo />
             <HeaderBar />
+            <AutoScrollToTop />
         </>
     );
 }
